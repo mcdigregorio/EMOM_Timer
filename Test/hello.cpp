0824b5a01f8b5d410cea66974ae0530ccf12d3fb
mcdigregorio <michael.c.digregorio@gmail.com>
2020-02-27 13:45:15 -0500

    hello.cpp initial commit

---
+++ Test/hello.cpp
@@ -0,0 +1,8 @@
+#getting started from https://wiki.qt.io/Getting_Started_on_the_Commandline
+
+#include <QTextStream>
+
+int main() {
+ QTextStream(stdout) << "Hello, world!" << endl;
+ return 0;
+}
